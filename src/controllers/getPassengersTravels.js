8b2441bbeec63da54c2ce659db31a23a9dd20dc6
Alicia T Nishiyama <nishiyamaeel@gmail.com>
2023-06-22 16:07:04 -0300

    get by name

--- src/controllers/getPassengersTravels.js
+++ src/controllers/getPassengersTravels.js
@@ -1,8 +1,10 @@
 import * as passengersTravelsService from "../services/passengersTravelsService.js"
 
 async function getPassengersTravels(req, res) {
+  const page = req.query.page
+  const name = req.query.name
   try {
-    const result = await passengersTravelsService.passengersTavrelsInfo()
+    const result = await passengersTravelsService.getTravelsService(page, name)
     res.status(200).send(result);
   } catch (error) {
     console.log(error);
--- src/services/passengersTravelsService.js
+++ src/services/passengersTravelsService.js
@@ -1,8 +1,33 @@
 import * as passengersTavrelsRepository from "../repositories/passengersTavrelsRepository.js"
+import httpStatus from "http-status"
 
-async function passengersTavrelsInfo(){
-const {rows} =  await passengersTavrelsRepository.findPassagersTravels()
-return rows 
+async function getTravelsService(page, name){
+  if(name === undefined && page === undefined){
+    return await getPassengersTravels();
+  } else if(name) {
+    return await getTravelsByPassengerName(name)
+  } else if(page) {
+    return await getTravelsByPage(page)
+  }
+}
+//return await
+
+async function getPassengersTravels(){
+  const {rows} =  await passengersTavrelsRepository.findPassagersTravels()
+  if(rows.length > 100) return res.status(httpStatus.INTERNAL_SERVER_ERROR); 
+  return rows
 }
 
-export { passengersTavrelsInfo }
+async function getTravelsByPassengerName(name){
+  const passengerTravels = await passengersTavrelsRepository.findTravelsByName(name)
+  if(passengerTravels.rows.length === 0) return send(httpStatus.NOT_FOUND) 
+  return passengerTravels.rows
+}
+
+async function getTravelsByPage(page){
+  const resultsByPage = await passengersTavrelsRepository.findTravelsByName(name)
+  if(resultsByPage.rows.length === 0) return send(httpStatus.NOT_FOUND)
+  return resultsByPage.rows
+}
+
+export { getTravelsService }
